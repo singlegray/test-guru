5e48fab278b46dfcc95efc12d80d7a3fb45dc937
singlegray <50613216+singlegray@users.noreply.github.com>
2020-06-13 21:40:35 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_31_175812) do
+ActiveRecord::Schema.define(version: 2020_06_13_182006) do
 
   create_table "answers", force: :cascade do |t|
+    t.integer "question_id", null: false
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "correct", default: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -26,18 +27,20 @@ ActiveRecord::Schema.define(version: 2020_05_31_175812) do
   end
 
   create_table "questions", force: :cascade do |t|
+    t.integer "test_id", null: false
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 1
+    t.integer "category_id", null: false
+    t.string "title"
+    t.integer "level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "categories_id", null: false
-    t.index ["categories_id"], name: "index_tests_on_categories_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,5 +49,7 @@ ActiveRecord::Schema.define(version: 2020_05_31_175812) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "tests", "categories", column: "categories_id"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
