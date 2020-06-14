4cfb4dddc7ff9661c62d3cfe6c69027184b03e38
singlegray <50613216+singlegray@users.noreply.github.com>
2020-06-14 16:15:13 +0300

    Update 20200614115304_create_answers.rb

--- db/migrate/20200614115304_create_answers.rb
+++ db/migrate/20200614115304_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
     create_table :answers do |t|
       t.references :question, null: false, foreign_key: true
       t.string :body, null: false
-      t.boolean :correct, default: 1
+      t.boolean :correct, default: false
 
       t.timestamps
     end
